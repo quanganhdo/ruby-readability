cea85dd29d424428f49128c9fe6270aa259012d4
Anh Do <hello@anh.do>
2022-01-30 02:16:52 -0500

    Added feature to get the largest image, and also grab image size from style attribute, if available

--- lib/readability.rb
+++ lib/readability.rb
@@ -17,7 +17,8 @@ module Readability
       :min_image_height           => 80,
       :ignore_image_format        => [],
       :blacklist                  => nil,
-      :whitelist                  => nil
+      :whitelist                  => nil,
+      :get_largest_image          => false
     }.freeze
     
     REGEXES = {
@@ -104,6 +105,9 @@ module Readability
 
       @best_candidate_has_image = false if reload
 
+      largest_image_url = nil
+      largest_image_area = 0
+
       prepare_candidates
       list_images   = []
       tested_images = []
@@ -118,6 +122,22 @@ module Readability
           url     = element["src"].value
           height  = element["height"].nil?  ? 0 : element["height"].value.to_i
           width   = element["width"].nil?   ? 0 : element["width"].value.to_i
+
+          if element["style"]
+
+            width_reg = /width:(\d+)/.match(element["style"])
+            height_reg = /height:(\d+)/.match(element["style"])
+
+            if width_reg
+              width = width_reg[1].to_i
+            end
+
+            if height_reg
+              height = height_reg[1].to_i
+            end
+            
+          end
+          
 
           if url =~ /\Ahttps?:\/\//i && (height.zero? || width.zero?)
             image   = get_image_size(url)
@@ -135,13 +155,27 @@ module Readability
 
           tested_images.push(url)
           if image_meets_criteria?(image)
-            list_images << url
+            if options[:get_largest_image]
+              area = image[:height] * image[:width]
+              if area > largest_image_area
+                largest_image_area = area
+                largest_image_url = url
+              end
+            else
+              list_images << url
+            end
+            
           else
             debug("Image discarded: #{url} - height: #{image[:height]} - width: #{image[:width]} - format: #{image[:format]}")
           end
         end
 
-      (list_images.empty? and content != @html) ? images(@html, true) : list_images
+        if options[:get_largest_image] and largest_image_url
+          list_images << largest_image_url
+        end
+
+        (list_images.empty? and content != @html) ? images(@html, true) : list_images
+
     end
     
     def images_with_fqdn_uris!(source_uri)
